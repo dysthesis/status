bfe150e2a3339b6cec5359c85556fa1982924482
Dysthesis <antheoraviel@protonmail.com>
2025-06-17 14:46:31 +1000

    Introduce the delimiter back

--- src/main.zig
+++ src/main.zig
@@ -6,6 +6,8 @@ const cpu = @import("cpu.zig");
 const clock = @import("clock.zig");
 const module = @import("module.zig");
 
+const tray_size = 4;
+
 pub fn main() !void {
     var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
     defer arena.deinit();
@@ -22,15 +24,18 @@ pub fn main() !void {
 
     while (true) {
         _ = arena.reset(.free_all);
-        var parts: [modules.len][]const u8 = undefined;
+
+        var parts: [(modules.len * 2)][]const u8 = undefined;
 
         for (modules, 0..) |m, i| {
-            parts[i] = m.fetch(m, alloc);
+            parts[i * 2] = m.fetch(m, alloc);
+            parts[(i * 2) + 1] = DELIM;
         }
 
         const status = try std.mem.concat(alloc, u8, parts[0..]);
+        const tray_padding = " " ** tray_size;
 
-        try out.print("{s}", .{status});
+        try out.print("{s}{s}", .{ status, tray_padding });
         try bw.flush();
         std.time.sleep(1_000_000_000);
     }
